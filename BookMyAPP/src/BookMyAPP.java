406f28a0c7da04c50ba92b06036572a222d4ed96
AswathydinNair <aswathydnair31@gmail.com>
2026-03-16 08:29:39 +0530

    message

---
+++ BookMyAPP/src/BookMyAPP.java
@@ -0,0 +1,6 @@
+public class BookMyAPP {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Hotel Booking Managemment System");
+        System.out.println("System initialized successfully.");
+    }
+}
